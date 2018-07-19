1a441540e703e338f1b6ef31812cc41773783f05
Manjot Chahal <mchahal22@gmail.com>
2018-07-19 16:45:59 -0600

    account model

---
+++ BuellerAPI/Bueller.API/Models/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bueller.API.Models
+{
+    public class Account
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(".{1,200}[@].{1,200}[.].{1,5}", ErrorMessage = "Email address cannot have more than 200 characters on each side of @")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(64, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 64 characters")]
+        public string Password { get; set; }
+    }
+}
