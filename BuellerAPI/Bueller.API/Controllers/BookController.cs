d1f5b09003fae8144aef1aab32640324b6069799
Manjot Chahal <mchahal22@gmail.com>
2018-08-02 16:59:06 -0600

    add book api endpoints/services

--- BuellerAPI/Bueller.API/Controllers/BookController.cs
+++ BuellerAPI/Bueller.API/Controllers/BookController.cs
@@ -1,6 +1,8 @@
 ﻿using Bueller.Data.Repositories;
+using Bueller.Library.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -38,6 +40,82 @@ namespace Bueller.API.Controllers
 
         [HttpGet]
         [AllowAnonymous]
+        [Route("GetById/{id}")]
+        public IHttpActionResult Get(int id)
+        {
+            var result = bookRepo.GetById(id);
+            if (result == null)
+            {
+                return Content(HttpStatusCode.NotFound, "Item does not exist");
+            }
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("Add", Name = "AddBook")]
+        public IHttpActionResult Post(Book bookDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            bookRepo.Insert(bookDto);
+
+            return CreatedAtRoute("AddClass", new { id = bookDto.BookId }, bookDto);
+        }
+
+        [HttpPut]
+        [AllowAnonymous]
+        [Route("Edit/{id}")]
+        public IHttpActionResult Edit(int id, Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != book.BookId)
+            {
+                return Content(HttpStatusCode.NotFound, "Item does not exist");
+            }
+
+            try
+            {
+                bookRepo.Update(book);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return Content(HttpStatusCode.NotFound, "Item does not exist");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [HttpDelete]
+        [AllowAnonymous]
+        [Route("Delete/{id}")]
+        public IHttpActionResult Delete(int id)
+        {
+            var result = bookRepo.GetById(id);
+            if (result == null)
+            {
+                return Content(HttpStatusCode.NotFound, "Item does not exist");
+            }
+
+            bookRepo.Delete(id);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
         [Route("GetBooksByClassId/{id}")]
         public IHttpActionResult GetBooksByClassId(int id)
         {
@@ -60,6 +138,20 @@ namespace Bueller.API.Controllers
                 return Content(HttpStatusCode.NotFound, "List is empty");
             }
             return Ok(classes);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("Assign/{classid}/{bookid}")]
+        public IHttpActionResult AssignBook(int classid, int bookid)
+        {
+            cross.AssignBook(classid, bookid);
+            return Ok();
+        }
+
+        private bool BookExists(int id)
+        {
+            return bookRepo.BookExists(id);
         }
     }
 }
