4a469478552b57c6b6061ff6afba5d92cbd83ffc
Manjot Chahal <mchahal22@gmail.com>
2018-08-06 11:51:14 -0600

    remove id in Book model

--- BuellerAngular/src/app/book.ts
+++ BuellerAngular/src/app/book.ts
@@ -1,7 +1,9 @@
+import { Class } from "./class";
+
 export class Book {
     BookId: number;
-    id: number;
     Title: string;
     Description: string;
     Price: number;
+    Classes: Class[];
 }
